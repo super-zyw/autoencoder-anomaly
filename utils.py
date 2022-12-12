ca450d3cd3ca21bf344ba9eb41f9487ae5781e2f
Yinwei Zhang <39347880+yinwei-zhang@users.noreply.github.com>
2022-12-11 17:18:37 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -27,6 +27,11 @@ class EarlyStopper:
         return False
 
 def construct_raw_memory_bank(train_val_list, use_cuda = False):
+    """
+    :param train_val_list: training sample list
+    :param use_cuda: use cuda
+    :return: raw_memory_bank, [nsamples, 128, 32, 32]
+    """
     kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
     train_val_loader = torch.utils.data.DataLoader(dataset.listDataset(train_val_list,
                                                                    shuffle=True,
@@ -39,52 +44,78 @@ def construct_raw_memory_bank(train_val_list, use_cuda = False):
     model.load_state_dict(torch.load('best_model.pt'))
     model.eval()
 
+    nsample = 1000
     # 1000 is the train set + val set
-    raw_memory_bank = torch.zeros((1000, 128, 32, 32))
+    raw_memory_bank = torch.zeros((nsample, 128, 32, 32))
     for train_idx, train_img in enumerate(train_val_loader):
+        if train_idx == nsample:
+            break
         feature, _ = model(train_img)
         if train_idx % 200 == 0:
             print('constructing memory bank, {}'.format(train_idx % 1000))
         raw_memory_bank[train_idx] = feature
     return raw_memory_bank
 
-def construct_memory_bank_with_nei(memory_bank, w):
+def construct_aggregate_memory_bank(memory_bank, w):
+    """
+    :param memory_bank: raw memory bank, [nsamples, 128, 32, 32]
+    :param w: window size
+    :return: aggreated memory bank, [nsample, 128 * w * w, 32, 32]
+    """
     shape = memory_bank.size()
-    new_bank = torch.zeros((shape[0], shape[1] * w * w, shape[2], shape[3]))
-
-    for i in range(shape[2] - w + 1):
-        for j in range(shape[3] - w + 1):
-            for k in range(shape[0]):
-                bank = memory_bank[k, :, i:i+w, j:j+w]
-                new_bank[k, :, i, j] = torch.reshape(bank, (-1,))
-
-    for i in range(shape[2] - w + 1, shape[2]):
-        for j in range(shape[3] - w + 1, shape[3]):
-            for k in range(shape[0]):
-                bank = memory_bank[k, :, i-w:i, j-w:j]
-                new_bank[k, :, i, j] = torch.reshape(bank, (-1,))
-    return new_bank
+    agg_bank = torch.zeros((shape[0], shape[1] * w * w, shape[2], shape[3]))
+    pads = (w//2, w//2)
+    dims = (1, 2)
+    for i in range(shape[0]):
+        raw_feature = memory_bank[i, :, :, :]
+        raw_feature = raw_feature.detach().numpy()
+        raw_feature = np.pad(raw_feature, ((0, 0), pads, pads), 'edge')
+        agg_feature = np.lib.stride_tricks.sliding_window_view(raw_feature, (w, w), dims)  # apply sliding window on 1 and 2 axis
+        agg_bank[i, :, :, :] = torch.Tensor(np.reshape(agg_feature, [shape[1] * w * w, shape[2], shape[3]]))
+    print('aggregate bank size: {}'.format(agg_bank.size()))
+    return agg_bank
 
 
-def get_new_feature(nei_feature, nei_memory_bank, raw_feature, raw_bank):
-    # extract_feature : [1, 128, 32, 32]
-    # memory_bank: [1000, 128, 32, 32]
+def get_new_feature(agg_feature, agg_bank, raw_feature, raw_bank):
+    """
+    :param agg_feature: aggreated feature for a test image, [1, 128 * w * w, 32, 32]
+    :param agg_bank: aggregated memory bank generated from training image, [nsamples, 128 * w * w, 32, 32]
+    :param raw_feature: raw feature from the encoder, [1, 128, 32, 32]
+    :param raw_bank: raw memory bank from training image , [nsamples, 128, 32, 32]
+    :return: new feature without anomaly, , [128, 32, 32]
+    """
+    agg_shape = agg_bank.size()
+    raw_shape = raw_bank.size()
 
-    shape = nei_memory_bank.size()
-    cos = nn.CosineSimilarity()
+    # vectorize the memory bank
+    raw_bank = raw_bank.view(raw_shape[0] * raw_shape[2] * raw_shape[3], raw_shape[1])
+    agg_bank = agg_bank.view(agg_shape[0] * agg_shape[2] * agg_shape[3], agg_shape[1])
 
-    avg_feature = torch.zeros((128, 32, 32))
-    for i in range(shape[2]):
-        for j in range(shape[3]):
-            sim_scores = [[0, 0]] * shape[0]
-            for k in range(shape[0]):
-                sim_score = cos(torch.unsqueeze(nei_feature[0, :, i, j], dim = 1), torch.unsqueeze(nei_memory_bank[k, :, i, j], 1))
-                sim_scores[k] = [sim_score.detach().numpy()[0], k]
-            sim_scores = sorted(sim_scores, key=lambda x: x[0], reverse = True)[:10]
-            scores = [score for index, score in sim_scores]
-            indexes = [index for index, score in sim_scores]
-            if np.mean(scores) < 0.8:
-                avg_feature[:, i, j] = torch.mean(raw_bank[indexes], dim = 0)[:, i, j]
+    # define cosine similarity
+    cos = nn.CosineSimilarity(dim = 1)
+
+    out_feature = torch.zeros((128, 32, 32))
+    for i in range(raw_shape[2]):
+        print('i: {}'.format(i))
+        for j in range(raw_shape[3]):
+            arr1 = agg_feature[0, :, i, j]
+
+            # add extra dimension, and repeat
+            arr1 = arr1[None, :]
+            arr1 = arr1.repeat(agg_shape[0] * agg_shape[2] * agg_shape[3], 1)
+
+            # calculate cosine similarity
+            sim_scores = cos(arr1, agg_bank)
+
+            # select top k similar features
+            scores, indexes = torch.topk(sim_scores, 10)
+
+            # update the feature
+            if torch.mean(scores) < 0.2:
+                out_feature[:, i, j] = torch.mean(raw_bank[indexes])
             else:
-                avg_feature[:, i, j] = raw_feature[0, :, i, j]
-    return avg_feature
+                out_feature[:, i, j] = raw_feature[0, :, i, j]
+
+    return out_feature
+
+
